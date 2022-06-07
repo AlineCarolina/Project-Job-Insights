2420624f1e1db928484ada36349dfe9fdd0b7559
Aline <eualine2017@gmail.com>
2022-06-06 21:49:23 -0300

    feat: implementação da função filter_by_salary_range

--- src/insights.py
+++ src/insights.py
@@ -67,44 +67,14 @@ def matches_salary_range(job, salary):
     if type(salary) != int:
         raise ValueError("4")
     return job["min_salary"] <= int(salary) <= job["max_salary"]
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    list_items = []
+    for index, job in enumerate(jobs):
+        try:
+            if (matches_salary_range(jobs[index], salary)) is True:
+                list_items.append(jobs[index])
+        except ValueError:
+            ...
+    return list_items
