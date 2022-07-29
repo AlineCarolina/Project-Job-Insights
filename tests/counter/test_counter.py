2e5d0d255c427278e480431c5e75d34d955431a7
Aline <eualine2017@gmail.com>
2022-07-29 14:56:40 -0300

    alteração

--- tests/counter/test_counter.py
+++
@@ -1,9 +0,0 @@
-from src.counter import count_ocurrences
-
-
-def test_counter():
-    counter = count_ocurrences("src/jobs.csv", "JavaScript")
-    assert counter == 122
-
-    counter = count_ocurrences("src/jobs.csv", "salesforce")
-    assert counter == 646
